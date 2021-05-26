a193735b7c931c012a70194895b327c36afaaf89
PapaDrago <oscarwaii@hotmail.com>
2021-05-26 01:14:40 -0500

    no es mucho
    pero es trabajo honesto

--- resources/js/Pages/QuienesSomos.jsx
+++ resources/js/Pages/QuienesSomos.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import '/css/QuienesSomos.css';
-import Paper from '@material-ui/core/Paper';
+import Layout from '../layouts/Layout';
 import Grid from '@material-ui/core/Grid';
-
+import Container from '@material-ui/core/Container';
 
 const QuienesSomos = () => {
     return (
         <>
-            <Grid width="xl" className={"Atma", "quienes_somos"}>
-                <div className={"content"}> hola hola soy comepasto :v</div>
-            </Grid>
+            <div className="Atma quienes_somos">
+                <div className={"quienes_somos_content"}> 
+                    <Container> 
+                        <Grid container direction="row" justify="flex-start">
+                            <Grid container direction="row" justify="flex-start" item xs={8}>
+                                <Grid item className="quienes_title ">¿Quiénes somos?</Grid>
+                                <Grid item className="quienes_title2">DISTRIBUIDOR VEGANO</Grid>
+                                <Grid item xs={12} className="quienes_subtitle">Distribuidor vegano de Morelia, Michoacán, México</Grid>
+                                <Grid item xs={12} className="quienes_info">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure
+                                </Grid>
+                            </Grid>
+                            <Grid container item xs={4} row justify="center" alignItems="center">
+                                <img className="quienes_img" src="/img/ilustraciones/In-no-time-amico-2.png" alt="" />
+                            </Grid>
+                        </Grid>
+                        <Grid container direction="row" justify="space-between" item xs={12}>
+                            <Grid item className="quienes_card">
+                                    hola
+                            </Grid>
+                            <Grid item className="quienes_card">
+                                    hola
+                            </Grid>
+                            <Grid item className="quienes_card">
+                                    hola
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </div>
+            </div>
         </>
     )
 }
+
+
+QuienesSomos.layout = page => <Layout children={page} title="Comepasto - ¿Quienes somos?" pageTitle="¿Quienes Somos?" />
 
 export default QuienesSomos
--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/quienes-somos', [App\Http\Controllers\quienesSomosController::class, 'index'])->name('quienesSomos');
+Route::get('/quienessomos', [App\Http\Controllers\quienesSomosController::class, 'index'])->name('quienesSomos');
