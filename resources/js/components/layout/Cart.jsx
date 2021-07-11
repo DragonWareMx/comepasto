c84c489a8f02508f9955485d60bd69e0e8b2407b
FernandoGASA <xpadrian2010@hotmail.com>
2021-07-11 01:25:45 -0500

    carrito vacio

--- resources/js/components/layout/Cart.jsx
+++ resources/js/components/layout/Cart.jsx
@@ -9,6 +9,7 @@ import { InertiaLink, usePage } from '@inertiajs/inertia-react';
 import RemoveIcon from '@material-ui/icons/Remove';
 import AddIcon from '@material-ui/icons/Add';
 import CloseSharpIcon from '@material-ui/icons/CloseSharp';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 
 const StyledBadge = withStyles((theme) => ({
     badge: {
@@ -218,6 +219,54 @@ const useStyles = makeStyles((theme) => ({
         padding: "0px",
         border: "none"
     },
+    carritoVacioTitulo:{
+        fontFamily: 'Atma',
+        fontStyle: 'normal',
+        fontWeight: '600',
+        fontSize: '20px',
+        lineHeight: '49px',
+        color: '#1DA3A8',
+    },
+    carritoVacio:{
+        color: '#626262',
+
+        fontFamily: 'Atma',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '16px',
+        lineHeight: '20px',
+        width: "90%",
+        textAlign: "center"
+    },
+    inertiaButtonCart: {
+        width: "fit-content",
+        height: "fit-content",
+        backgroundColor: "transparent",
+        marginTop: "12px",
+        marginBottom: "20px",
+        padding: "0px",
+        border: "none"
+    },
+    buttonCart: {
+        background: 'transparent',
+        color: '#9F9F9F',
+
+        fontFamily: 'Atma',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: '13px',
+        lineHeight: '21px',
+
+        width: '100%',
+        height: '38px',
+
+        border: "1px solid #E3E3E3",
+
+        '&:hover': {
+            background: '#1DA3A8',
+            color: '#FFFFFF',
+        },
+    },
   }));
 
 export default function Footer() {
@@ -227,6 +276,7 @@ export default function Footer() {
     const [open, setOpen] = React.useState(false);
     const [dialog, setDialog] = React.useState(false);
 
+    //CLICK EN EL ICONO DEL CARRITO, si el usuario inicio sesion se muestra el contenido del carrito, si no se abre el dialog
     function handleClick(event){
         if(auth && auth.cart){
             setOpen(true);
@@ -299,7 +349,7 @@ export default function Footer() {
     }, [flash])
 
     return (
-        <>
+    <>
         {/* ICONO DEL CARRITO */}
         <IconButton aria-label="cart" className={classes.cartbutton} onClick={handleClick}>
             <StyledBadge badgeContent={auth.cart ? calcularCantidadDeProductos(auth.cart) : 0} color="primary">
@@ -307,7 +357,7 @@ export default function Footer() {
             </StyledBadge>
         </IconButton>
 
-        {/* CARD DEL CARRITO, AQUI SE MUESTRAN LOS ELEMENTOS EN EL CARRITO */}
+        {/* CARD DEL CARRITO, AQUI SE MUESTRAN LOS ELEMENTOS EN EL CARRITO SI EL USUARIO HA INICIADO SESION */}
         {auth && auth.user && auth.cart &&
         <Drawer
             anchor="right"
@@ -340,7 +390,7 @@ export default function Footer() {
 
 
                 {/* PRODUCTOS DEL CARRITO */}
-                {auth.cart.length > 0 && auth.cart.map(producto => (
+                {auth.cart.length > 0 ? auth.cart.map(producto => (
                 <>
                     {/* PRODUCTO */}
                     <Grid container style={{maxWidth: 320, margin: 10}}>
@@ -448,181 +498,73 @@ export default function Footer() {
                     </Grid>
 
                     <Divider variant="middle" />
+
+                    {/* TOTAL */}
+                    <Grid item xs={12} container justify="space-between">
+                        <Grid item>
+                            <Typography gutterBottom className={classes.precioTotal} align="left">
+                                Total:
+                            </Typography>
+                        </Grid>
+
+                        <Grid item>
+                        <Typography gutterBottom className={classes.precioTotal} align="right">
+                            {calcularTotal(auth.cart)}
+                        </Typography>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container justify="center" direction="column">
+                        <Grid container item justify="center" xs={12}> 
+                            <InertiaLink href="/ejemplo" style={{textDecoration: "none"}} className={classes.buttonGrid}>
+                                <Button variant="contained" color="primary" disableElevation className={classes.button}>
+                                    Proceder pago
+                                </Button>
+                            </InertiaLink>
+                        </Grid>
+
+                        <Grid container item className={classes.linkcotizar} justify="center">
+                            <InertiaLink style={{color: "#595959"}} href="/ejemplo">
+                                Cotizar costo de envío
+                            </InertiaLink>
+                        </Grid>
+                    </Grid>
                 </>
                 ))
-                }
-                {/* TOTAL */}
-                <Grid item xs={12} container justify="space-between">
+                :
+                <>
+                {/* CARRITO VACIO */}
+                <Grid container 
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    style={{width: 250, margin: 10}}
+                >
                     <Grid item>
-                        <Typography gutterBottom className={classes.precioTotal} align="left">
-                            Total:
-                        </Typography>
+                        <ShoppingCartOutlinedIcon style={{ fontSize: 50, color: "#1DA3A8" }} />
                     </Grid>
 
                     <Grid item>
-                    <Typography gutterBottom className={classes.precioTotal} align="right">
-                        {calcularTotal(auth.cart)}
-                    </Typography>
+                        <div className={classes.carritoVacioTitulo} style={{marginTop: "0px"}}>
+                            TU CARRITO ESTÁ VACÍO
+                        </div>
+                        <div className={classes.carritoVacio} style={{marginTop: "0px"}}>
+                            Agrega productos a tu carrito
+                        </div>
                     </Grid>
-                </Grid>
 
-                <Grid container justify="center" direction="column">
-                    <Grid container item justify="center" xs={12}> 
-                        <InertiaLink href="/ejemplo" style={{textDecoration: "none"}} className={classes.buttonGrid}>
-                            <Button variant="contained" color="primary" disableElevation className={classes.button}>
-                                Proceder pago
+                    <Grid item>
+                        <InertiaLink href={route('inicio')} as="button" style={{textDecoration: "none"}} className={classes.inertiaButtonCart} preserveScroll>
+                            <Button variant="contained" color="primary" component="div" disableElevation className={classes.buttonCart}>
+                                IR A LA TIENDA
                             </Button>
                         </InertiaLink>
                     </Grid>
-
-                    <Grid container item className={classes.linkcotizar} justify="center">
-                        <InertiaLink style={{color: "#595959"}} href="/ejemplo">
-                            Cotizar costo de envío
-                        </InertiaLink>
-                    </Grid>
-                </Grid>
+                 </Grid>
+                 </>
+                }
             </div>
         </Drawer>
-        // <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
-        //     {/* Detecta si se hizo click afuera del card para cerrar el carrito */}
-        //     <ClickAwayListener onClickAway={handleClickAway}>
-        //         <Card className={classes.card}>
-        //             {/* Icono para cerrar el carrito */}
-        //             <CardHeader 
-        //             action={
-        //                 <IconButton aria-label="close" size="small" onClick={handleClickAway}>
-        //                     <CloseSharpIcon fontSize="small"/>
-        //                 </IconButton>
-        //             } />
-                    
-        //             {/* PRODUCTOS DEL CARRITO */}
-        //             {auth.cart.length > 0 && auth.cart.map(producto => (
-        //             <CardContent>
-        //                 {/* PRODUCTO */}
-        //                 <Grid container spacing={2}>
-        //                     {/* Imagen del producto */}
-        //                     <Grid item xs={3}>
-        //                         <ButtonBase>
-        //                             {producto.foto &&
-        //                             <img className={classes.img} alt="complex" src={"/storage/products/" + producto.foto} />
-        //                             }
-        //                         </ButtonBase>
-        //                     </Grid>
-
-        //                     <Grid item xs container>
-        //                         {/* Nombre del producto */}
-        //                         <Grid item xs={12}>
-        //                             <InertiaLink href="/producto" style={{textDecoration: "none"}}>
-        //                                 <Typography gutterBottom className={classes.nombreProducto}>
-        //                                     {producto.name.length > 100 ?
-        //                                     producto.name.substring(0, 100 - 3) + "..."
-        //                                     : 
-        //                                     producto.name
-        //                                     }
-        //                                 </Typography>
-        //                             </InertiaLink>
-        //                         </Grid>
-
-        //                         {/* Precios */}
-        //                         <Grid item xs={12} container spacing={1}>
-        //                             <Grid item >
-        //                                 <Typography gutterBottom className={classes.precioDescuento} align="left">
-        //                                     {"$"+producto.precio+" MXN"}
-        //                                 </Typography>
-        //                             </Grid>
-        //                             <Grid item >
-        //                                 <Typography gutterBottom  className={classes.precio} align="left">
-        //                                     {calcularDescuento(producto.precio, producto.descuento)}
-        //                                 </Typography>
-        //                             </Grid>
-        //                         </Grid>
-                                
-        //                         {/* Cantidad */}
-        //                         <Grid item xs={12} container spacing={1}>
-        //                             <Grid item xs container alignItems="center" justify="flex-end">
-        //                                 <Typography gutterBottom className={classes.cantidad} align="right">
-        //                                     Cantidad
-        //                                 </Typography>
-        //                             </Grid>
-
-        //                             <Grid item xs container justify="flex-end">
-        //                                 <OutlinedInput type="number"
-        //                                 className={classes.inputSinFlechas}
-        //                                 endAdornment={
-        //                                     <InputAdornment position="end">
-        //                                     <IconButton
-        //                                         aria-label="add"
-        //                                     >
-        //                                         <AddIcon fontSize="small" />
-        //                                     </IconButton>
-        //                                     </InputAdornment>
-        //                                 }
-        //                                 startAdornment={
-        //                                     <InputAdornment position="start">
-        //                                     <IconButton
-        //                                         aria-label="remove"
-        //                                     >
-        //                                         <RemoveIcon fontSize="small" />
-        //                                     </IconButton>
-        //                                     </InputAdornment>
-        //                                 }
-                                        
-        //                                 value={producto.pivot.cantidad} />
-        //                             </Grid>
-        //                         </Grid>
-                                    
-        //                         {/* Precio */}
-        //                         <Grid item xs={12}>
-        //                             <Typography gutterBottom className={classes.precioProducto} align="right">
-        //                                 {producto.pivot.cantidad+"x$"+calcularTotalProducto(producto).toFixed(2)}
-        //                             </Typography>
-        //                         </Grid>
-        //                     </Grid>
-        //                 </Grid>
-                        
-        //                 <Divider variant="middle" />
-
-        //                 {/* TOTAL */}
-        //                 <Grid item xs={12} container justify="space-between">
-        //                     <Grid item>
-        //                         <Typography gutterBottom className={classes.precioTotal} align="left">
-        //                             Total:
-        //                         </Typography>
-        //                     </Grid>
-
-        //                     <Grid item>
-        //                     <Typography gutterBottom className={classes.precioTotal} align="right">
-        //                         {calcularTotal(auth.cart)}
-        //                     </Typography>
-        //                     </Grid>
-        //                 </Grid>
-
-        //             </CardContent>
-        //             ))
-        //             }
-
-
-        //             {/* BOTONES DE PAGO Y ENVIO */}
-        //             <CardActions>
-        //                 <Grid container justify="center" direction="column">
-        //                     <Grid container item justify="center" xs={12}> 
-        //                         <InertiaLink href="/ejemplo" style={{textDecoration: "none"}} className={classes.buttonGrid}>
-        //                             <Button variant="contained" color="primary" disableElevation className={classes.button}>
-        //                                 Proceder pago
-        //                             </Button>
-        //                         </InertiaLink>
-        //                     </Grid>
-
-        //                     <Grid container item className={classes.linkcotizar} justify="center">
-        //                         <InertiaLink style={{color: "#595959"}} href="/ejemplo">
-        //                             Cotizar costo de envío
-        //                         </InertiaLink>
-        //                     </Grid>
-        //                 </Grid>
-        //             </CardActions>
-        //         </Card>
-        //     </ClickAwayListener>
-        // </Popper>
         }
 
         {/* DIALOG QUE SE MUESTRA CUANDO EL USUARIO NO HA INICIADO SESION */}
@@ -652,6 +594,6 @@ export default function Footer() {
             </div>
         </Dialog>
 
-        </>
+    </>
     );
 }
