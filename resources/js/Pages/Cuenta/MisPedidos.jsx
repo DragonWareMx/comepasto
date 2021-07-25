7ae70e960d1362c5c3dc65f8e38ed8e53357bb7b
Dulce G Marín <66979331+DulceGaby@users.noreply.github.com>
2021-07-25 00:58:31 -0500

    cosas chafa

--- resources/js/Pages/Cuenta/MisPedidos.jsx
+++ resources/js/Pages/Cuenta/MisPedidos.jsx
@@ -21,8 +21,40 @@ const MisPedidos = () => {
                         <InertiaLink href={route('cuenta')} style={{color:'#1DA3A8', textDecoration:'none'}}>CUENTA</InertiaLink>&nbsp;<Grid style={{fontWeight:300}}>MIS PEDIDOS</Grid>
                     </Grid>
                 
-                    <Grid container direction="row" justify="space-between" style={{marginTop:20}}>
-                        PEDIDOS
+                    <Grid container direction="row" style={{marginTop:20}}>
+
+                        <Grid item xs={12} className="grid-container-pedido">
+                            <Grid item xs={12} className="grid-gray-info">
+
+                                <Grid item className="grid-child-gray" direction="column" alignItems="center">
+                                    <Grid className="header-title">PEDIDO REALIZADO</Grid>
+                                    <Grid className="header-info">24 Mayo 2021</Grid>
+                                </Grid>
+
+                                <Grid item className="grid-child-gray" direction="column" alignItems="center">
+                                    <Grid className="header-title">TOTAL</Grid>
+                                    <Grid className="header-info">$7005.58 MXN</Grid>
+                                </Grid>
+
+                                <Grid item className="grid-child-gray" direction="column" alignItems="center">
+                                    <Grid className="header-title">TIPO DE ENTREGA</Grid>
+                                    <Grid className="header-info">Envío a domicilio</Grid>
+                                </Grid>
+
+                                <Grid item className="grid-child-gray" direction="column" alignItems="center">
+                                    <Grid className="header-title">TIPO DE PAGO</Grid>
+                                    <Grid className="header-info">Pago en efectivo</Grid>
+                                </Grid>
+                            </Grid>
+                            
+                            <Grid item xs={12} className="grid-white">
+                                <Grid xs={12} className="status-pedido">En espera de entrega</Grid>
+                                <Grid>Costos de envío a Dirección completa lorem ipsum dolor sit amet consecteur $90.00 MXN</Grid>
+                            </Grid>
+
+                        </Grid>
+
+
                     </Grid>
                 </Grid>
             </Grid>
