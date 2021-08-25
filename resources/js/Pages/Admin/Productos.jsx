7fae1283c3d3c81c0ac62a6c766d90148a0dc680
Dulce G Marín <66979331+DulceGaby@users.noreply.github.com>
2021-08-24 21:40:03 -0500

    va a estar machizo configurar la tabla

--- resources/js/Pages/Admin/Productos.jsx
+++ resources/js/Pages/Admin/Productos.jsx
@@ -14,7 +14,7 @@ import Tooltip from '@material-ui/core/Tooltip';
 import InputBase from '@material-ui/core/InputBase';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { DataGrid } from '@material-ui/data-grid';
+import {DataGrid}  from '@material-ui/data-grid';
 
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
@@ -32,9 +32,6 @@ const useStyles = makeStyles((theme) => ({
       marginRight: theme.spacing(2),
       marginLeft: 0,
       width: '100%',
-    //   [theme.breakpoints.up('sm')]: {
-    //     width: 'auto',
-    //   },
     },
     searchIcon: {
       padding: theme.spacing(0, 2),
@@ -67,6 +64,52 @@ const useStyles = makeStyles((theme) => ({
     //   },
     },
   }));
+
+const columns = [
+// { field: 'id', headerName: 'ID', width: 90 },
+{
+    field: 'firstName',
+    headerName: 'Nombre',
+    width: 150,
+    editable: false,
+},
+{
+    field: 'lastName',
+    headerName: 'Marca',
+    width: 150,
+    editable: false,
+},
+{
+    field: 'age',
+    headerName: 'Precio',
+    type: 'number',
+    width: 110,
+    editable: false,
+},
+{
+    field: 'fullName',
+    headerName: 'Descuento',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+    `${params.getValue(params.id, 'firstName') || ''} ${
+        params.getValue(params.id, 'lastName') || ''
+    }`,
+},
+];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
 
 
 const Productos = () => {
@@ -172,6 +215,15 @@ const Productos = () => {
                                 <MenuItem onClick={handleClose}>Atributos</MenuItem>
                             </Menu>
                         </Grid>
+                    </Grid>
+                    <Grid item xs={12} style={{height:'300px'}}>
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        pageSize={5}
+                        rowsPerPageOptions={[5]}
+                        disableSelectionOnClick
+                    />
                     </Grid>
 
                 </Grid>
