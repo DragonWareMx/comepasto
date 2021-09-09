b7513d16e8960e33f21b646e4de192488c28d159
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-09-09 00:37:37 -0500

    Acabé la parte de cuenta

--- app/Http/Controllers/CuentaController.php
+++ app/Http/Controllers/CuentaController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Sale;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Hash;
 
 class CuentaController extends Controller
 {
@@ -30,7 +31,44 @@ class CuentaController extends Controller
 
     public function informacion()
     {
+        if (Auth::guest()) {
+            return redirect()->route('inicio')->with('info', 'Por favor primero inicia sesión.');
+        }
         return Inertia::render('Cuenta/Informacion');
+    }
+
+    public function infoEdit(Request $request)
+    {
+        //dd($request->all());
+        if ($request->password) {
+            $validated = $request->validate([
+                'correo' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . Auth::id()],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+                'telefono' => ['required', 'numeric', 'digits: 10', 'unique:users,tel,' . Auth::id()],
+            ]);
+        } else {
+            $validated = $request->validate([
+                'correo' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . Auth::id()],
+                'telefono' => ['required', 'numeric', 'digits: 10', 'unique:users,tel,' . Auth::id()],
+            ]);
+        }
+        DB::beginTransaction();
+        try {
+            $usuario = User::findOrFail(Auth::id());
+            $usuario->email = $request->correo;
+            $usuario->tel = $request->telefono;
+            if ($request->password) {
+                $usuario->password = Hash::make($request->password);
+            }
+            $usuario->save();
+
+            DB::commit();
+            return redirect()->back()->with('success', 'Tu información se ha actualizado correctamente.');
+        } catch (\Throwable $th) {
+            //throw $th;
+            DB::rollBack();
+            return redirect()->back()->with('error', 'Ocurrió un error inesperado, intentalo más tarde.');
+        }
     }
 
     public function direcciones()
