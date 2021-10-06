209ea71f6857a2f302474aa95f00393d9f2057fb
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-10-06 16:26:10 -0500

    Llegó el momento
    De intentar lo de sorteable :'v

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Banner;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Product;
@@ -17,79 +18,84 @@ use Illuminate\Support\Str;
 
 class AdminController extends Controller
 {
-    public function index(){
-        $productos=Product::
-            leftJoin('brands','products.brand_id','=','brands.id')
-            ->select('products.id','products.name as nombre','brands.name as marca', 'precio' , 'descuento', 'stock')
+    public function index()
+    {
+        $productos = Product::leftJoin('brands', 'products.brand_id', '=', 'brands.id')
+            ->select('products.id', 'products.name as nombre', 'brands.name as marca', 'precio', 'descuento', 'stock')
             ->selectRaw('(`products`.`precio` - `products`.`precio`*(`products`.`descuento`/100)) AS precio_descuento')
             ->get();
-        
-        $total=$productos->count();
 
-        $sinStock=$productos->where('stock','<=',0)->count();
+        $total = $productos->count();
 
-        $stock=$productos->sum('stock');
+        $sinStock = $productos->where('stock', '<=', 0)->count();
 
-        $totalProductos=DB::table('products')->sum(DB::raw('stock * precio'));
-        
-            return Inertia::render('Admin/Productos/Productos',[
-            'total'=>$total,
-            'sinStock'=>$sinStock,
-            'stock'=>$stock,
-            'totalProductos'=>$totalProductos,
-            'productos'=>$productos,
+        $stock = $productos->sum('stock');
+
+        $totalProductos = DB::table('products')->sum(DB::raw('stock * precio'));
+
+        return Inertia::render('Admin/Productos/Productos', [
+            'total' => $total,
+            'sinStock' => $sinStock,
+            'stock' => $stock,
+            'totalProductos' => $totalProductos,
+            'productos' => $productos,
         ]);
     }
 
-    public function producto($id){
-        $producto=Product::with('brand:name,id','type:id,name','category:id,name','img:product_id,url')->findOrFail($id);
-        return Inertia::render('Admin/Productos/Producto',[
-            'producto'=>$producto,
+    public function producto($id)
+    {
+        $producto = Product::with('brand:name,id', 'type:id,name', 'category:id,name', 'img:product_id,url')->findOrFail($id);
+        return Inertia::render('Admin/Productos/Producto', [
+            'producto' => $producto,
         ]);
     }
 
-    public function productoEditar($id){
-        $producto=Product::with('brand:id,name')
+    public function productoEditar($id)
+    {
+        $producto = Product::with('brand:id,name')
             ->with('type:id,name')
             ->with('category:id,name')
             ->findOrFail($id);
-        $marcas=Brand::select('id','name')->get();
-        $tipos=Type::select('id','name')->get();
-        $categorias=Category::select('id','name')->get();
-        return Inertia::render('Admin/Productos/EditarProducto',[
-            'producto'=>$producto,
-            'marcas'=>$marcas,
-            'tipos'=>$tipos,
-            'categorias'=>$categorias,
+        $marcas = Brand::select('id', 'name')->get();
+        $tipos = Type::select('id', 'name')->get();
+        $categorias = Category::select('id', 'name')->get();
+        return Inertia::render('Admin/Productos/EditarProducto', [
+            'producto' => $producto,
+            'marcas' => $marcas,
+            'tipos' => $tipos,
+            'categorias' => $categorias,
         ]);
     }
 
-    public function productoPatch(Request $request, $id){
-        dd('holi we',$id,$request);
+    public function productoPatch(Request $request, $id)
+    {
+        dd('holi we', $id, $request);
     }
 
-    public function productoAgregar(){
-        $marcas=Brand::select('id','name')->get();
-        $tipos=Type::select('id','name')->get();
-        $categorias=Category::select('id','name')->get();
-        return Inertia::render('Admin/Productos/AgregarProducto',[
-            'marcas'=>$marcas,
-            'tipos'=>$tipos,
-            'categorias'=>$categorias,
+    public function productoAgregar()
+    {
+        $marcas = Brand::select('id', 'name')->get();
+        $tipos = Type::select('id', 'name')->get();
+        $categorias = Category::select('id', 'name')->get();
+        return Inertia::render('Admin/Productos/AgregarProducto', [
+            'marcas' => $marcas,
+            'tipos' => $tipos,
+            'categorias' => $categorias,
         ]);
     }
 
-    public function storeProducto(Request $request){
+    public function storeProducto(Request $request)
+    {
         $validated = $request->validate([
             'foto' => 'required|image|mimes:jpeg,png,jpg,gif|max:51200',
-            'nombre' => ['required','max:100','regex:/^[A-Za-z0-9À-ÖØ-öø-ÿ_! \"#$%&\'()*+,\-.\\:\/;=?@^_]+$/'],
+            'nombre' => ['required', 'max:100', 'regex:/^[A-Za-z0-9À-ÖØ-öø-ÿ_! \"#$%&\'()*+,\-.\\:\/;=?@^_]+$/'],
             'marca' => 'required|exists:brands,id',
             'tipo' => 'required|exists:types,id',
             'categoria' => 'required|exists:categories,id',
-            'presentacion' => ['nullable','max:250','regex:/^[A-Za-z0-9À-ÖØ-öø-ÿ_! \"#$%&\'()*+,\-.\\:\/;=?@^_]+$/'],
+            'presentacion' => ['nullable', 'max:250', 'regex:/^[A-Za-z0-9À-ÖØ-öø-ÿ_! \"#$%&\'()*+,\-.\\:\/;=?@^_]+$/'],
             'precio' => 'required|between:0,999999.99|numeric',
             'descuento' => 'required|between:0,100.00|numeric',
-            'ingredientes' => ['nullable','max:8000','regex:/^[A-Za-z0-9À-ÖØ-öø-ÿ_! \"#$%&\'()*+,\-.\\:\/;=?@^_]+$/'],
+            'ingredientes' => ['nullable', 'max:8000', 'regex:/^[A-Za-z0-9À-ÖØ-öø-ÿ_! \"#$%&\'()*+,\-.\\:\/;=?@^_]+$/'],
             'soyaFree' => 'required|boolean',
             'trigoFree' => 'required|boolean',
         ]);
@@ -108,7 +114,7 @@ class AdminController extends Controller
             //guarda la foto
             $foto = $request->file('foto')->store('public/products');
             $newProduct->foto = $request->file('foto')->hashName();
-            
+
             //informacion
             $newProduct->uuid = Str::uuid();
             $newProduct->name = $request->nombre;
@@ -123,180 +129,187 @@ class AdminController extends Controller
             $newProduct->trigoFree = $request->trigoFree;
 
             $newProduct->save();
-            
+
             //REDIRECCIONA A LA VISTA DE PRODUCTOS
-            return \Redirect::route('admin.productos')->with('success','El producto ha sido registrado con éxito!');
+            return \Redirect::route('admin.productos')->with('success', 'El producto ha sido registrado con éxito!');
         } catch (\Exception $e) {
             DB::rollBack();
 
             //si hay foto se elimina del servidor
-            if($foto)
-            {
+            if ($foto) {
                 \Storage::delete($foto);
             }
 
-            return \Redirect::back()->with('error','Ha ocurrido un error al intentar registrar el producto, inténtelo más tarde.');
+            return \Redirect::back()->with('error', 'Ha ocurrido un error al intentar registrar el producto, inténtelo más tarde.');
         }
     }
 
-    public function productoInventario(Request $request, $id){
+    public function productoInventario(Request $request, $id)
+    {
         $validated = $request->validate([
             'stock' => 'required|numeric',
         ]);
-        $product=Product::findOrFail($id);
+        $product = Product::findOrFail($id);
         try {
             DB::beginTransaction();
-            $product->stock=$request->stock;
+            $product->stock = $request->stock;
             $product->save();
             DB::commit();
-            return \Redirect::back()->with('success','Stock actualizado con éxito.');
+            return \Redirect::back()->with('success', 'Stock actualizado con éxito.');
         } catch (\Throwable $th) {
             dd($th);
             DB::rollback();
-            return \Redirect::back()->with('error','Ocurrió un problema, vuelva a intentarlo más tarde.');
+            return \Redirect::back()->with('error', 'Ocurrió un problema, vuelva a intentarlo más tarde.');
         }
     }
 
-    public function pedidos(){
-        $pedidos=Sale::
-            leftJoin('users','sales.client_id','=','users.id')
-            ->select('sales.id','sales.created_at as fecha','users.name as cliente', 'sales.total as total','tipo_entrega as entrega')
-            ->orderBy('id','DESC')
-        ->get();
+    public function pedidos()
+    {
+        $pedidos = Sale::leftJoin('users', 'sales.client_id', '=', 'users.id')
+            ->select('sales.id', 'sales.created_at as fecha', 'users.name as cliente', 'sales.total as total', 'tipo_entrega as entrega')
+            ->orderBy('id', 'DESC')
+            ->get();
 
-        $total=$pedidos->count();
+        $total = $pedidos->count();
 
-        $ganancias=$pedidos->sum('total');
-        
-        return Inertia::render('Admin/Pedidos/Pedidos',[
-            'total'=>$total,
-            'ganancias'=>$ganancias,
-            'pedidos'=>$pedidos,
+        $ganancias = $pedidos->sum('total');
+
+        return Inertia::render('Admin/Pedidos/Pedidos', [
+            'total' => $total,
+            'ganancias' => $ganancias,
+            'pedidos' => $pedidos,
         ]);
     }
 
-    public function pedido($id){
-        $pedido=Sale::
-            with('client','product')
-        ->findOrFail($id);
-        return Inertia::render('Admin/Pedidos/Pedido',[
-            'pedido'=>$pedido,
+    public function pedido($id)
+    {
+        $pedido = Sale::with('client', 'product')
+            ->findOrFail($id);
+        return Inertia::render('Admin/Pedidos/Pedido', [
+            'pedido' => $pedido,
         ]);
     }
 
-    public function pedidosAgregar(){
+    public function pedidosAgregar()
+    {
         return Inertia::render('Admin/Pedidos/AgregarPedido');
     }
 
-    public function clientes(){
+    public function clientes()
+    {
         $clients = User::leftJoin('sales', 'sales.client_id', '=', 'users.id')
-                        ->selectRaw('users.id, name AS nombre,tel AS telefono, email AS correo, users.created_at AS registro, SUM(sales.total) AS total')
-                        ->groupBy('users.id', 'users.name', 'users.tel', 'users.email', 'users.created_at')
-                        ->get();
+            ->selectRaw('users.id, name AS nombre,tel AS telefono, email AS correo, users.created_at AS registro, SUM(sales.total) AS total')
+            ->groupBy('users.id', 'users.name', 'users.tel', 'users.email', 'users.created_at')
+            ->get();
 
         return Inertia::render('Admin/Clientes/Clientes', [
             'clients' => $clients
         ]);
     }
 
-    public function cliente($id){
-        $cliente=User::with('sale','sale.product')->findOrFail($id);
-        return Inertia::render('Admin/Clientes/Cliente',[
-            'cliente'=>$cliente,
+    public function cliente($id)
+    {
+        $cliente = User::with('sale', 'sale.product')->findOrFail($id);
+        return Inertia::render('Admin/Clientes/Cliente', [
+            'cliente' => $cliente,
         ]);
     }
 
-    public function preguntas(Request $request){
-        if($request->busqueda){
-            $preguntas=Question::
-            where('question','like','%'.$request->busqueda.'%')
-            ->orWhere('answer','like','%'.$request->busqueda.'%')
-            ->orWhere('type','like','%'.$request->busqueda.'%')
-            ->get();
+    public function preguntas(Request $request)
+    {
+        if ($request->busqueda) {
+            $preguntas = Question::where('question', 'like', '%' . $request->busqueda . '%')
+                ->orWhere('answer', 'like', '%' . $request->busqueda . '%')
+                ->orWhere('type', 'like', '%' . $request->busqueda . '%')
+                ->get();
+        } else {
+            $preguntas = Question::get();
         }
-        else{
-            $preguntas=Question::get();
-        }
-        return Inertia::render('Admin/Preguntas/Preguntas',[
-            'preguntas'=>$preguntas,
+        return Inertia::render('Admin/Preguntas/Preguntas', [
+            'preguntas' => $preguntas,
         ]);
     }
 
-    public function preguntasEliminar($id){
+    public function preguntasEliminar($id)
+    {
         try {
             DB::beginTransaction();
-            $pregunta=Question::findOrFail($id);
+            $pregunta = Question::findOrFail($id);
             $pregunta->delete();
             DB::commit();
-            return \Redirect::back()->with('success','Pregunta eliminada con éxito.');
-
+            return \Redirect::back()->with('success', 'Pregunta eliminada con éxito.');
         } catch (\Throwable $th) {
-           DB::rollback();
-           return \Redirect::back()->with('error','Ocurrió un problema, vuelva a intentarlo más tarde.');
+            DB::rollback();
+            return \Redirect::back()->with('error', 'Ocurrió un problema, vuelva a intentarlo más tarde.');
         }
     }
 
-    public function preguntasCreate(Request $request){
+    public function preguntasCreate(Request $request)
+    {
         $validated = $request->validate([
-            'pregunta' => ['required', 'string','max:255'],
+            'pregunta' => ['required', 'string', 'max:255'],
             'respuesta' => ['required', 'string'],
             'categoria' => ['required', 'numeric'],
         ]);
         try {
             DB::beginTransaction();
-            $pregunta=new Question;
-            $pregunta->question=$request->pregunta;
-            $pregunta->answer=$request->respuesta;
-            $pregunta->type=$request->categoria;
+            $pregunta = new Question;
+            $pregunta->question = $request->pregunta;
+            $pregunta->answer = $request->respuesta;
+            $pregunta->type = $request->categoria;
             $pregunta->save();
             DB::commit();
-            return \Redirect::back()->with('success','Pregunta creada con éxito.');
-
+            return \Redirect::back()->with('success', 'Pregunta creada con éxito.');
         } catch (\Throwable $th) {
-           DB::rollback();
-           return \Redirect::back()->with('error','Ocurrió un problema, vuelva a intentarlo más tarde.');
+            DB::rollback();
+            return \Redirect::back()->with('error', 'Ocurrió un problema, vuelva a intentarlo más tarde.');
         }
     }
 
-    public function banners(){
-        return Inertia::render('Admin/Banners/Banners');
+    public function banners()
+    {
+        $banners = Banner::get();
+        return Inertia::render('Admin/Banners/Banners', ['banners' => $banners]);
     }
 
-    public function recetas(Request $request){
-        if($request->busqueda){
-            $recetas=Recipe::where('recipes.nombre','like','%'.$request->busqueda.'%')
-            ->orWhere('imgs.descripcion','like','%'.$request->busqueda.'%')
-            ->join('imgs','recipes.id', '=', 'imgs.recipe_id')
-                ->select('recipes.*', 'imgs.*')->orderBy('recipes.created_at','desc')->get();
+    public function recetas(Request $request)
+    {
+        if ($request->busqueda) {
+            $recetas = Recipe::where('recipes.nombre', 'like', '%' . $request->busqueda . '%')
+                ->orWhere('imgs.descripcion', 'like', '%' . $request->busqueda . '%')
+                ->join('imgs', 'recipes.id', '=', 'imgs.recipe_id')
+                ->select('recipes.*', 'imgs.*')->orderBy('recipes.created_at', 'desc')->get();
+        } else {
+            $recetas = Recipe::join('imgs', 'recipes.id', '=', 'imgs.recipe_id')
+                ->select('recipes.*', 'imgs.*')->orderBy('recipes.created_at', 'desc')->get();
         }
-        else{
-            $recetas=Recipe::join('imgs','recipes.id', '=', 'imgs.recipe_id')
-                ->select('recipes.*', 'imgs.*')->orderBy('recipes.created_at','desc')->get();
-        }
-            
-        return Inertia::render('Admin/Recetas/Recetas',[
+
+        return Inertia::render('Admin/Recetas/Recetas', [
             'recetas' => $recetas,
         ]);
     }
 
-    public function receta($id){
-        $receta=Recipe::join('imgs','recipes.id', '=', 'imgs.recipe_id')
+    public function receta($id)
+    {
+        $receta = Recipe::join('imgs', 'recipes.id', '=', 'imgs.recipe_id')
             ->select('recipes.*', 'imgs.*')->findOrFail($id);
 
-        $productos=DB::table('product_recipe')
+        $productos = DB::table('product_recipe')
             ->join('products', 'product_recipe.product_id', '=', 'products.id')
             ->join('categories', 'products.category_id', '=', 'categories.id')
             ->select('products.name', 'products.foto', 'products.id', 'products.uuid', 'categories.name as categoria')
             ->where('recipe_id', '=', $id)->get();
 
-        return Inertia::render('Admin/Recetas/Receta',['receta' => $receta, 'productos' => $productos]);
+        return Inertia::render('Admin/Recetas/Receta', ['receta' => $receta, 'productos' => $productos]);
     }
 
-    public function recetaEditar($id){
+    public function recetaEditar($id)
+    {
         return Inertia::render('Admin/Recetas/EditarReceta');
     }
 
-    public function recetasAgregar(){
+    public function recetasAgregar()
+    {
         return Inertia::render('Admin/Recetas/AgregarReceta');
     }
 }
--- resources/js/Pages/Admin/Banners/Banners.jsx
+++ resources/js/Pages/Admin/Banners/Banners.jsx
@@ -19,102 +19,78 @@ import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     paper: {
-    //   padding: theme.spacing(2),
-      textAlign: 'center',
-      border:'none !important',
-      boxShadow:'none !important',
-    //   backgroundColor:'black',
-      height: '240px',
+        //   padding: theme.spacing(2),
+        textAlign: 'center',
+        border: 'none !important',
+        boxShadow: 'none !important',
+        //   backgroundColor:'black',
+        height: '240px',
     },
-  }));
+    delete: {
+        "&:hover": {
+            color: "red"
+        }
+    }
+}));
 
-function deleteImg(){
+function deleteImg() {
     document.getElementById("example-img").remove();
 }
 
-const Banners = () => {
+const Banners = ({ banners }) => {
     const classes = useStyles();
-
-    return ( 
+    console.log(banners)
+    return (
         <>
-        <Container> 
-            <Grid container style={{paddingTop:'34px'}}>
-                {/* TOP PAGE, TITTLE AND ADD BUTTON */}
-                <Grid item xs={12} className="top-admin">
-                    <Grid className="title-page">BANNERS</Grid>
-                </Grid> 
+            <Container>
+                <Grid container style={{ paddingTop: '34px' }}>
+                    {/* TOP PAGE, TITTLE AND ADD BUTTON */}
+                    <Grid item xs={12} className="top-admin">
+                        <Grid className="title-page">BANNERS</Grid>
+                    </Grid>
 
-                {/* CONTENIDO GENERAL */}
-                <Grid item xs={12} style={{marginBottom:'25px', borderRadius:'4px', border:'1px solid #E1E3EA',padding:'20px'}}>
-                   
-                    
-                    <Grid container spacing={3} alignItems="stretch">
-                        <Grid item xs={6} sm={3}>
-                            <Paper className={classes.paper} style={{backgroundColor:'transparent'}}>
-                                 <input
-                                    accept="image/*"
-                                    id="contained-button-file"
-                                    type="file"
-                                    style={{display:'none'}}
-                                />
-                                <label htmlFor="contained-button-file" style={{marginTop:'20px'}}>
-                                    <Button variant="contained" className="button-add button-banner" startIcon={<PublishIcon />} component="span">
-                                    AGREGAR BANNER
-                                    </Button>
-                                </label>
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={6} sm={3} id="example-img">
-                            <Paper className={classes.paper}>
-                                <IconButton 
-                                    aria-label="delete" 
-                                    onClick={deleteImg}
-                                    style={{position:'absolute',zIndex:'999',color:'white'}}>
-                                    <DeleteIcon />
-                                </IconButton>
-                                <img src="/img/carrousel/1.png" className="banner" />
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Paper className={classes.paper}>
-                                <IconButton 
-                                    aria-label="delete" 
-                                    style={{position:'absolute',zIndex:'999',color:'white'}}>
-                                    <DeleteIcon />
-                                </IconButton>
-                                <img src="/img/carrousel/fondo6.jpg" className="banner" />
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Paper className={classes.paper}>
-                                <IconButton 
-                                    aria-label="delete" 
-                                    style={{position:'absolute',zIndex:'999',color:'white'}}>
-                                    <DeleteIcon />
-                                </IconButton>
-                                <img src="/img/carrousel/12.png" className="banner" />
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={6} sm={3}>
-                            <Paper className={classes.paper}>
-                                <IconButton 
-                                    aria-label="delete" 
-                                    style={{position:'absolute',zIndex:'999',color:'white'}}>
-                                    <DeleteIcon />
-                                </IconButton>
-                                <img src="/img/carrousel/8.png" className="banner" />
-                            </Paper>
+                    {/* CONTENIDO GENERAL */}
+                    <Grid item xs={12} style={{ marginBottom: '25px', borderRadius: '4px', border: '1px solid #E1E3EA', padding: '20px', marginBottom: '50px' }}>
+
+
+                        <Grid container spacing={3} alignItems="stretch">
+                            <Grid item xs={6} sm={3}>
+                                <Paper className={classes.paper} style={{ backgroundColor: 'transparent' }}>
+                                    <input
+                                        accept="image/*"
+                                        id="contained-button-file"
+                                        type="file"
+                                        style={{ display: 'none' }}
+                                    />
+                                    <label htmlFor="contained-button-file" style={{ marginTop: '20px' }}>
+                                        <Button variant="contained" className="button-add button-banner" startIcon={<PublishIcon />} component="span">
+                                            AGREGAR BANNER
+                                        </Button>
+                                    </label>
+                                </Paper>
+                            </Grid>
+                            {banners && banners.length > 0 && banners.map((banner, index) => (
+                                <Grid item xs={6} sm={3} key={index}>
+                                    <Paper className={classes.paper}>
+                                        <IconButton
+                                            aria-label="delete"
+                                            style={{ position: 'absolute', zIndex: '999', color: 'white' }}>
+                                            <DeleteIcon className={classes.delete} />
+                                        </IconButton>
+                                        <img src={"/img/carrousel/" + banner.url} className="banner" />
+                                    </Paper>
+                                </Grid>
+                            ))}
                         </Grid>
                     </Grid>
+
                 </Grid>
-                
-            </Grid>
-        </Container>
-        
-    </>
+            </Container>
+
+        </>
     )
 }
 
